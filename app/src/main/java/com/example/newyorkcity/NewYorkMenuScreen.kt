ecc5c5c44c5336e974d431b9d49021c95ef1d5fe
Joshua <joshuagutierrez724@gmail.com>
2023-08-17 15:14:23 -0400

    First Screen basic layout

--- app/src/main/java/com/example/newyorkcity/NewYorkMenuScreen.kt
+++ app/src/main/java/com/example/newyorkcity/NewYorkMenuScreen.kt
@@ -1,12 +1,26 @@
 package com.example.newyorkcity
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import com.example.newyorkcity.model.LocationModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 
@@ -25,18 +39,41 @@ fun BoroughList(
     boroughs: List<LocationModel>,
     modifier: Modifier = Modifier
 ){
-    LazyColumn{
-        items(boroughs) {
-            BoroughLayout(location = it)
+
+        LazyColumn {
+            items(boroughs) {
+                BoroughLayout(location = it)
+            }
         }
-    }
+
 }
 
 @Composable
 fun BoroughLayout(
-    location: LocationModel
+    location: LocationModel,
+    modifier: Modifier = Modifier
 ) {
-    Row(){
-
+    Card(
+        elevation = CardDefaults.cardElevation(dimensionResource(R.dimen.small_padding)),
+        modifier = modifier.padding(dimensionResource(R.dimen.medium_padding))
+    ){
+        Column(
+        ) {
+            Image(
+                painter = painterResource(location.locationImage),
+                contentDescription = null,
+                //modifier.weight(1F)
+            )
+            //Spacer(modifier.weight(1F))
+            Text(
+                text = stringResource(location.locationName),
+                style = MaterialTheme.typography.headlineSmall,
+                textAlign = TextAlign.Center,
+                modifier = modifier.padding(start = dimensionResource(R.dimen.medium_padding),
+                    top = dimensionResource(R.dimen.small_padding),
+                    bottom = dimensionResource(R.dimen.small_padding)
+                )
+            )
+        }
     }
 }
--- app/src/main/java/com/example/newyorkcity/ui/theme/Type.kt
+++ app/src/main/java/com/example/newyorkcity/ui/theme/Type.kt
@@ -2,33 +2,33 @@ package com.example.newyorkcity.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.newyorkcity.R
 
 // Set of Material typography styles to start with
+val libreBaserville = FontFamily(
+    Font(R.font.libre_baskerville_regular),
+    Font(R.font.libre_baskerville_bold),
+    Font(R.font.libre_baskerville_italic)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    displayLarge = TextStyle(
+        fontFamily = libreBaserville,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 36.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    headlineSmall = TextStyle(
+        fontFamily = libreBaserville,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = libreBaserville,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
     )
-    */
 )
