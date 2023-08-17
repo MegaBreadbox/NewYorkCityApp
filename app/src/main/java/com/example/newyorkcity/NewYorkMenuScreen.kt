1887aee2de11e8b7fe8b55df8aa4bb10080e2bc9
Joshua <joshuagutierrez724@gmail.com>
2023-08-17 12:51:15 -0400

    Added colorScheme

--- app/src/main/java/com/example/newyorkcity/NewYorkMenuScreen.kt
+++ app/src/main/java/com/example/newyorkcity/NewYorkMenuScreen.kt
@@ -1,15 +1,23 @@
 package com.example.newyorkcity
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import com.example.newyorkcity.model.LocationModel
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun CompactMenuScreen() {
-    val viewModel: NewYorkViewModel
+    val viewModel: NewYorkViewModel = viewModel()
+    val uiState by viewModel.uiState.collectAsState()
 
+    BoroughList(
+        boroughs = uiState.boroughs
+    )
 }
 
 @Composable
@@ -28,5 +36,7 @@ fun BoroughList(
 fun BoroughLayout(
     location: LocationModel
 ) {
+    Row(){
 
+    }
 }
